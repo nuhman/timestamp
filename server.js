aba85aebac157f0a1e80d5fea00c0e4b0b3b94b6
nuhman <nuhman7@outlook.com>
2017-12-04 12:13:06 +0530

    add server.js and index.ejs code

---
+++ server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+
+app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.json());
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    //res.sendFile(__dirname + '\\index.html');
+    res.render("index.ejs", {name: 'nuhman'});
+});
+
+var monthNames = ["January", "February", "March", "April", "May", "June",
+"July", "August", "September", "October", "November", "December"
+];
+
+
+app.get("/:date", (req, res) => {
+    //res.sendFile(__dirname + '\\index.html');
+    var param = urldecode(req.params.date);
+    param = param.trim();    
+    param = param.split('-').length > 1 ? param.split('-') : param;
+    // dates with '-' follows 'yyyy-mm-dd' however url is mm-dd-yyyy
+    var date, natural, unix;
+    if(typeof param === "string"){   
+        if (param.match(/[a-z]/i) || /\s/g.test(param)) { // does it contain alphabets or spaces in between?
+            date = new Date(param);    
+        } else{
+            date = new Date(param * 1000);
+            unix = param;
+        }     
+        
+    } else {
+        date = new Date(param[2], param[0] - 1, param[1]);        
+    }         
+    natural =  monthNames[date.getMonth()] + " " + date.getDate() +", " + date.getFullYear();
+    if(!unix){
+        unix = Date.parse( monthNames[date.getMonth()] + " " + date.getDate()+", " + date.getFullYear() );
+        unix = unix/1000;
+    }
+    var data = {"unix":  unix, "natural": natural};
+    data = JSON.parse(JSON.stringify(data));    
+    console.log(typeof data);
+    // it is a date
+    if ( isNaN( date.getTime() ) ) {  // d.valueOf() could also work
+        // date is not valid
+        re.send(null);
+    }
+    else {
+        // date is valid        
+        res.send(data);
+    }          
+});
+
+app.listen(3000, () => {
+    console.log("Listening on 3000...");
+});
+
+console.log('May Node be with you');
+
+
+function urldecode(str) {
+    return decodeURIComponent((str+'').replace(/\+/g, '%20'));
+ }
